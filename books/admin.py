301e308fcf6883b1a1873c1adafcf27e0ec213df
liufei <liufei@alijk.com>
2015-05-15 09:32:44 +0800

    增加第六章《Django站点管理》学习代码。

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
+# coding=utf-8
 
-# Register your models here.
+from django.contrib import admin
+from books.models import Publisher, Author, Book
+
+
+"""
+1.将models加入到Admin管理中
+2.ModelAdmin类可对Model类管理进行定制
+"""
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date' # 在管理界面上对日期字段进行层次划分
+    ordering = ('-publication-date', )
+    # 算定义编辑表单字段
+    fields = ('title', 'authors', 'publisher', 'publication_date')
+    # 自定义Many-to-Many字段的编辑，使用前先将fields注释掉
+    filter_horizontal = ('authors',)
+    # 将外键字段展现为文本框而非下拉框，提高数据加载和展示效率
+    raw_id_fields = ('publisher', )
+
+
+admin.site.register(Publisher)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -16,19 +17,21 @@ class Publisher(models.Model):
         '''
         return self.name
 
+
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, verbose_name="e-mail")
 
     def __unicode__(self):
         return u'{0} {1}'.format(self.first_name, self.last_name)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)    # 与Author是多对多关系
     publisher = models.ForeignKey(Publisher)    # 外键，对应Publisher
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)  # 允许为空
 
     def __unicode__(self):
         return self.title
